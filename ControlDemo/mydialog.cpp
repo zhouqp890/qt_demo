8c2a8c8062f83b88163590e9cdf28917841bba80
周启鹏 <zhouqp@corp.21cn.com>
2021-03-01 23:49:17 +0800

    feat: 增加控件demo

---
+++ ControlDemo/mydialog.cpp
@@ -0,0 +1,15 @@
+#include "mydialog.h"
+#include "./ui_mydialog.h"
+
+MyDialog::MyDialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::MyDialog)
+{
+    ui->setupUi(this);
+}
+
+MyDialog::~MyDialog()
+{
+    delete ui;
+}
+
